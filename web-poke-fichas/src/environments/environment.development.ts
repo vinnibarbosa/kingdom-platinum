8ead0624015d3d13793d27585e41394895379967
Vinnícius <vinniciusbarbosa9@gmail.com>
2026-08-03 12:30:06 -0300

    Consulta catalogo Pokemon diretamente no Supabase

--- web-poke-fichas/src/environments/environment.development.ts
+++ web-poke-fichas/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8080/api',
-  supabasePokemonUrl: '',
-  supabasePokemonAnonKey: '',
+  supabasePokemonUrl: 'https://vnccljhhexlsthjaaorr.supabase.co',
+  supabasePokemonAnonKey: 'sb_publishable_qv49Mr15vgXo1AqMuwWEAA_9ComxNNn',
   supabasePokemonTable: 'pokemons',
   supabaseMoveTable: 'moves',
   supabasePokemonLimit: 2000
--- web-poke-fichas/src/environments/environment.ts
+++ web-poke-fichas/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://p01--kingdom-platinum--y7pyslnljzqn.code.run/api',
-  supabasePokemonUrl: '',
-  supabasePokemonAnonKey: '',
+  supabasePokemonUrl: 'https://vnccljhhexlsthjaaorr.supabase.co',
+  supabasePokemonAnonKey: 'sb_publishable_qv49Mr15vgXo1AqMuwWEAA_9ComxNNn',
   supabasePokemonTable: 'pokemons',
   supabaseMoveTable: 'moves',
   supabasePokemonLimit: 2000
